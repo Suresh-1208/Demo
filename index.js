4b549186667efbcc9ee4001bd6fe2e1b6b5f71a0
Suresh-1208 <sureshbabuk1208@gmail.com>
2025-11-25 01:38:16 +0530

    index file is commited

---
+++ index.js
@@ -0,0 +1,13 @@
+// script.js
+
+// Run once the page loads
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Page loaded!");
+
+    const header = document.querySelector("header");
+
+    // Example interaction
+    header.addEventListener("click", () => {
+        alert("You clicked the header!");
+    });
+});
